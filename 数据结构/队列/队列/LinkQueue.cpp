5f83588ac46a4a9293a253a25f4b5757c69bf84d
dqzj <1056538693@qq.com>
2018-08-16 08:42:23 +0800

    单链表

---
+++ 数据结构/队列/队列/LinkQueue.cpp
@@ -0,0 +1,27 @@
+#include"LinkQueue.h"
+#include<string>
+int main()
+{
+	LinkQueue LQ;
+	InitQueue(LQ);
+	cout<<QueueEmpty(LQ)<<endl;
+	int i = 10;
+	while (i--)
+	{
+		EnQueue(LQ, i);
+	}
+	cout<<QueueLength(LQ)<<endl;
+	QueueTraverse(LQ);
+	
+	i = 5;
+	while (i--)
+	{
+		DeQueue(LQ);
+	}
+	cout << QueueLength(LQ) << endl;
+	QueueTraverse(LQ);
+
+	ClearQueue(LQ);
+	cout << QueueEmpty(LQ) << endl;
+	DesreoyQueue(LQ);
+}
